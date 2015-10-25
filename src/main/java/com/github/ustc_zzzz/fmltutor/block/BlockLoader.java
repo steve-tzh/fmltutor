b44379f58b3dba1e43499cd7bcac3b7c537305da
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2015-10-25 22:30:16 +0800

    v1.4

---
+++ src/main/java/com/github/ustc_zzzz/fmltutor/block/BlockLoader.java
@@ -0,0 +1,39 @@
+package com.github.ustc_zzzz.fmltutor.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameData;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class BlockLoader
+{
+    public static Block grassBlock = new BlockGrassBlock();
+
+    public BlockLoader(FMLPreInitializationEvent event)
+    {
+        register(grassBlock, "grass_block");
+    }
+
+    @SideOnly(Side.CLIENT)
+    public static void registerRenders()
+    {
+        registerRender(grassBlock);
+    }
+
+    private static void register(Block block, String name)
+    {
+        GameRegistry.registerBlock(block, name);
+    }
+
+    @SideOnly(Side.CLIENT)
+    private static void registerRender(Block block)
+    {
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(block), 0,
+                new ModelResourceLocation(GameData.getBlockRegistry().getNameForObject(block).toString(), "inventory"));
+    }
+}
