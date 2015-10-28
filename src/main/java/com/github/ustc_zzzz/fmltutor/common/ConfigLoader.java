e45b750eb8d7ad20553b4f02985cb7a1f3630127
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2015-10-28 19:17:13 +0800

    v1.8

---
+++ src/main/java/com/github/ustc_zzzz/fmltutor/common/ConfigLoader.java
@@ -0,0 +1,26 @@
+package com.github.ustc_zzzz.fmltutor.common;
+
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class ConfigLoader
+{
+    private static Configuration config;
+
+    public static int diamondBurnTime;
+
+    public ConfigLoader(FMLPreInitializationEvent event)
+    {
+        config = new Configuration(event.getSuggestedConfigurationFile());
+        config.load();
+
+        registerConfig();
+
+        config.save();
+    }
+
+    private static void registerConfig()
+    {
+        diamondBurnTime = config.get(Configuration.CATEGORY_GENERAL, "diamondBurnTime", 640).getInt();
+    }
+}
