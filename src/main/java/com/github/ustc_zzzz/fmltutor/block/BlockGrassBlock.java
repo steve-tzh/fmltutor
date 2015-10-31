7e4852c346a176023b98ad573c7a327eb0dca88a
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2015-10-31 11:10:20 +0800

    v2.1.1

--- src/main/java/com/github/ustc_zzzz/fmltutor/block/BlockGrassBlock.java
+++ src/main/java/com/github/ustc_zzzz/fmltutor/block/BlockGrassBlock.java
@@ -3,6 +3,9 @@ package com.github.ustc_zzzz.fmltutor.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockGrassBlock extends Block
 {
@@ -13,5 +16,14 @@ public class BlockGrassBlock extends Block
         this.setHardness(0.5F);
         this.setStepSound(soundTypeGrass);
         this.setCreativeTab(CreativeTabs.tabBlock);
+    }
+
+    @Override
+    public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn)
+    {
+        if (!worldIn.isRemote)
+        {
+            worldIn.setBlockToAir(pos);
+        }
     }
 }
